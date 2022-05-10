956604225eaa2faefd544db2ae5caa8041c21f71
Ttislevoll <torgeir.tislevoll@gmail.com>
2022-05-10 18:24:33 +0200

    MongoDB implemented

--- api/main.py
+++ api/main.py
@@ -3,7 +3,7 @@ from flask import Flask, request
 import requests
 from dotenv import load_dotenv
 from flask_cors import CORS
-
+from mongo_client import insert_test_document
 
 load_dotenv(dotenv_path="./.env.local")
 
@@ -19,6 +19,8 @@ app = Flask(__name__)
 CORS(app)
 
 app.config["DEBUG"] = DEBUG
+
+insert_test_document()
 
 
 @app.route("/new-image")
